501714a543f3f4955b7e7f1bbb28f792000c598d
Zavodil <support@zavodil.ru>
2021-03-19 16:47:39 +0200

    Bridge frontend link updated

--- packages/frontend/src/nearconfig.ts
+++ packages/frontend/src/nearconfig.ts
@@ -2,5 +2,5 @@ export const nearConfig = {
 	ClaimUrl: "https://wallet.testnet.near.org/create/testnet/",
 	MinAmountEthText: "0.05 ETH",
 	MinAmountEth: 50000000000000000,
-	BridgeUrl: "http://bridge.near.org/"
+	BridgeUrl: "https://ethereum.bridgetonear.org/"
 };
